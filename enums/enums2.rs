a905c274008d7493bab218ebbc16c13d07aa3cea
jpp46 <jpowers4@uvm.edu>
2019-12-05 08:23:00 -0500

    first commit

---
+++ enums/enums2.rs
@@ -0,0 +1,29 @@
+// enums2.rs
+// Make me compile! Execute `rustlings hint enums2` for hints!
+
+#[derive(Debug)]
+enum Message {
+    Move { x: i64, y: i64 },
+    Echo(String),
+    ChangeColor(i64, i64, i64),
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", &self);
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Move { x: 10, y: 30 },
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
---
+++ functions/functions1.rs
@@ -0,0 +1,10 @@
+// functions1.rs
+// Make me compile! Execute `rustlings hint functions1` for hints :)
+
+fn call_me() {
+    println!("This works now!")
+}
+
+fn main() {
+    call_me();
+}
